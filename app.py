fabf918744b0acc198a800e6fa5ff6427b8dc31f
Krish Rajpal <krishrajpal3@gmail.com>
2024-04-15 13:24:46 +0530

    New App

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import cv2
 import json
 import os
 from PIL import Image
-import tensorflow as tf
+import tensorflow as tf``
 import pandas as pd
 
 filename_mapping = {
@@ -13,11 +13,13 @@ filename_mapping = {
 }
 csv_file_path = "Scleral Spur Coordiantes_Manual  - Sheet1.csv"
 df = pd.read_csv(csv_file_path)
-def extract_image_info(filename):
-    # Extract image number and side information from filename
+def extract_image_info(filename): # Extract image number and side information from filename
     parts = filename.split('_')
     image_number = int(parts[0])  # Extract image number
-    side = parts[-1]  # Extract side information (left_half or mirrored_right_half)
+    if 'left' in filename:
+        side = 'left'
+    elif 'mirrored_right' in filename:
+        side = 'right'
     return image_number, side
 
 def load_model(model_path):
@@ -42,12 +44,12 @@ def calculate_new_coordinates(original_x, original_y):
 def main():
     st.title("Accurate localization of key points Scleral Spur based on VGG19 on ACA and measurement of relevant")
     uploaded_image = st.file_uploader("Upload an image", type=["png"])
-    filename = uploaded_image.name
-    image_number, side = extract_image_info(filename)
 
     if uploaded_image is not None:
         image_pil_1 = Image.open(uploaded_image)
         image_pil = image_pil_1.save("img.png")
+        filename = uploaded_image.name
+        image_number, side = extract_image_info(filename)
 
         st.image(image_pil_1, caption='Uploaded Image', width=300, channels='BGR')
         filename = os.path.basename(uploaded_image.name)
@@ -58,18 +60,18 @@ def main():
                 output_image = cv2.imread("output/" + output_image_filename)
                 st.image(output_image, caption='Predicted Image', use_column_width=True, channels='BGR')
             else:
+                print(side)
                 image_opencv = cv2.imread("img.png")
                 x_2 = np.array([image_opencv])
                 x_2 = x_2.astype('float32') / 255.0
                 x, y = predict_coordinates(x_2)
                 if(side == "left"):
-                    x_1 = df.iloc[image_number, 1]
-                    y_1 = df.iloc[image_number,2]
-                    cv2.circle(image_opencv, (int(x_1), int(y_1)), 2, (255,0,255), -1)
+                    x_left = df.iloc[image_number, 1]
+                    y_left = df.iloc[image_number,2]
+                    cv2.circle(image_opencv, (int(x_left), int(y_left)), 3, (0,165,255), -1)
                 else:
-                    x_1 = df.iloc[image_number+1, 1]
-                    y_1 = df.iloc[image_number+1,2]
-                    cv2.circle(image_opencv, (int(x_1), int(y_1)), 2, (255,0,255), -1)
+                    x_right, y_right = calculate_new_coordinates(df.iloc[image_number+1,3], df.iloc[image_number+1,4])
+                    cv2.circle(image_opencv, (int(x_right), int(y_right)), 3, (0,165,255), -1)
                 st.write(f"Predicted Coordinates: ({x}, {y})")
                 color = (255, 255, 255)  # White color
                 radius = 2
